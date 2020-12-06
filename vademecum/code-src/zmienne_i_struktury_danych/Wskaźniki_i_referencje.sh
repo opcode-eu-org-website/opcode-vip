4845d09498565427a949466f30305d28c11b58c6
Robert Paciorek <robert@opcode.eu.org>
2020-12-06 11:44:13 +0000

    poprawki o eval w bashu

--- vademecum/code-src/zmienne_i_struktury_danych/Wskaźniki_i_referencje.sh
+++ vademecum/code-src/zmienne_i_struktury_danych/Wskaźniki_i_referencje.sh
@@ -4,22 +4,31 @@
 # może pełnić zmienna zawierająca nazwę
 # innej zmiennej
 
-A="tekst do wypisania";
+A='tekst do wypisania, $HOME, `ls`';
 B="A";
 
 # proste podejście typu echo ${$B}
 # (działające np. w PHP) nie zadziała,
 # ale można to zrobić na kilka innych sposobów:
 
-C=${!B};
+C=${!B}; # ta metoda nie działa w czystym sh
 echo $C
 
-C='eval "echo \$$B"';
-D=`eval "$C"`
-echo $D
-
-C=$(C='eval "echo \$$B"'; eval $C);
+export A
+C=$(echo "\$$B" | envsubst)
+# ta metoda wymaga zewnętrznego polecenia
+# envsubst i wyeksporotwania zmiennych
 echo $C
+
+C=$( eval "echo \$$B" )
+echo $C
+
+# z użyciem większej liczby poleceń eval możemy
+# zapewnić także podstawienie kolejnego poziomu
+# zmiennych lub wykonanie wpisanych w nich poleceń
+C=$( eval eval "echo \$$B" )
+echo $C
+
 
 # zmienna może przechowywać komendę / nazwę
 # funkcji do wykonania
