aab469790a4a86cfa35a3900f46dcae049aed475
Robert Paciorek <robert@opcode.eu.org>
2024-10-01 20:26:51 +0000

    poprawki błędów

--- vademecum/code-src/podstawy/Skoki_warunki_pętle_funkcje.py
+++ vademecum/code-src/podstawy/Skoki_warunki_pętle_funkcje.py
@@ -13,9 +13,9 @@ else:
 	print("i>=j AND j>=k")
 
 # podstawowe operatory logiczne
-if (i<j or j<k)
-	puts("i<j OR j<k");
-# innymi operatorami logicznymi są and oraz not
+if i<j or j<k:
+	print("i<j OR j<k")
+# innymi operatorami logicznymi są `and` oraz `not`
 
 
 # pętla for
@@ -26,19 +26,19 @@ for i in range(2, 9):
 	if i==7:
 		# wyjście z pętli
 		break;
-	print(" a:", i);
+	print(" a:", i)
 else:
 	# nie wejdzie tutaj bo pętla kończy
 	# się z użyciem break
-	print("else w pętli");
+	print("else w pętli")
 
 # pętla while
 while i>0 :
 	i = i - 1;
-	print(" b:", i);
+	print(" b:", i)
 else:
 	# wejdzie tu gdy warunek i>0
 	# nie będzie spełniony
 	# zarówno przy pierwszym
 	# jak i kolejnych sprawdzeniach
-	print("else w pętli");
+	print("else w pętli")
